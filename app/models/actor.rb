7d7debd85856a85bd979327603b194e7520b0c87
Rashad Dixon <dixoncodes@gmail.com>
2020-01-21 12:33:01 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    full = "#{self.first_name} #{self.last_name}"
+    full
+  end
+
+  def list_roles
+    self.characters.map{|character| "#{character.name} - #{character.show.name}"}
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_show(attributes)
+    self.show = Show.new(attributes)
+  end
+
+  def build_network(attributes)
+    self.show.network = Network(attributes)
+  end
 end
